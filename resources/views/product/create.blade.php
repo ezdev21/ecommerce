4d38df5f4a97b6c6b77cfd656cf114e53b47c5db
gech11 <getachewfikadu77@gmail.com>
2021-04-24 18:01:44 +0300

    initial commit

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -5,7 +5,9 @@
      <p>add product here</p>
      <input type="text" name="name" placeholder="product name">
      <input type="decimal" placeholder="product cost">
-     <input type="file">
+     <input type="file" name="photo">
+     <textarea name="description" id="" cols="30" rows="10"></textarea>
+     <input type="submit" value="sell product">
   </form>    
  </div>   
 @endsection
