21cd427c58e54967786e09dcd4cae80b586b58ed
gech11 <getachewfikadu77@gmail.com>
2021-04-24 17:46:47 +0300

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <ul>
+    <li>All</li>
+    @foreach ($categories as $category)
+      <li>{{$category->title}}</li>    
+    @endforeach
+    </ul>
+    <div>
+      @forelse ($category->products as $product)
+          <div>
+              <img src="'products'.{{$product->id}}" alt="">
+              {{$product->name}}
+              {{$product->price}}
+          </div>
+      @empty
+          <p>oops no products were found</p>
+      @endforelse
     </div>
 </div>
 @endsection
