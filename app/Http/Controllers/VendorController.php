ccd400932100fa51944f17606f09c8efb2d21195
gech11 <getachewfikadu77@gmail.com>
2022-01-23 14:04:21 -0500

    vendors included

---
+++ app/Http/Controllers/VendorController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVendorRequest;
+use App\Http\Requests\UpdateVendorRequest;
+use App\Models\Vendor;
+
+class VendorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreVendorRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreVendorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vendor  $vendor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vendor $vendor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vendor  $vendor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vendor $vendor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateVendorRequest  $request
+     * @param  \App\Models\Vendor  $vendor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateVendorRequest $request, Vendor $vendor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vendor  $vendor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vendor $vendor)
+    {
+        //
+    }
+}
