997fd268e3445013711c7fc1a2db66f3de5af585
gech11 <getachewfikadu77@gmail.com>
2022-01-24 08:29:00 -0500

    product store feature included

---
+++ database/seeders/StoreSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class StoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
