53720cd658fe720a22ff3db70ea50253513e39b5
ezra02 <ezrafikadu02@gmail.com>
2022-11-25 22:50:50 +0300

    cart test implemented using phpunit

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -17,7 +17,7 @@ class CartController extends Controller
     public function __construct(){
 
       $this->middleware('auth');
-      
+
     }
 
     public function cartItems(Request $request)
@@ -28,6 +28,7 @@ class CartController extends Controller
       $cartItems=$cart->products;
       return response()->json(['cartItems'=>$cartItems]);
     }
+
     public function addToCart(Request $request)
     {
       $user=User::find($request->userId);
@@ -55,76 +56,6 @@ class CartController extends Controller
           $productInCart=true;
         }
       }
-      return response()->json(['productInCart'=>$productInCart]);  
-    }
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Cart  $cart
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Cart $cart)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Cart  $cart
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Cart $cart)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Cart  $cart
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Cart $cart)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Cart  $cart
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Cart $cart)
-    {
-        //
+      return response()->json(['productInCart'=>$productInCart]);
     }
 }
---
+++ tests/Feature/CartTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CartTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_only_authenticated_users_have_previliges_related_to_cart()
+    {
+        $response = $this->get('/profile/*')->whereIn('*',['create','store','edit','update']);
+        $response->assertRedirect('/login');
+    }
+
+    public function test_cart_items_of_authenticated_user_can_be_listed()
+    {
+        $user=User::factory()->create(['id'=>1]);
+        $this->actingAs($user);
+        $cart=Cart::factory()->create(['user_id'=>$user->id]);
+        $response=$this->get('/cartitems');
+        $response->assertJson('cartItems');
+    }
+
+    public function test_authenticated_user_can_add_product_to_cart()
+    {
+        $user=User::factory()->create(['id'=>1]);
+        $this->actingAs($user);
+        $product=Product::factory()->create();
+        $response=$this->post('cart/addproduct',[
+          'userId'=>$user->id,
+          'productId'=>$product->id
+        ]);
+        $response->assertOk();
+        $this->assertDatabaseCount('cart_product',1);
+    }
+
+    public function test_authenticated_user_can_remove_product_from_cart()
+    {
+        $user=User::factory()->create(['id'=>1]);
+        $this->actingAs($user);
+        $product=Product::factory()->create();
+        $cart=Cart::factory()->create(['user_id'=>$user->id]);
+        $response=$this->post('cart/addproduct',[
+          'userId'=>$user->id,
+          'productId'=>$product->id
+        ]);
+        $response->assertOk();
+        $this->assertDatabaseCount('cart_product',1);
+        $response=$this->post('cart/removeproduct',[
+          'userId'=>$user->id,
+          'productId'=>$product->id
+        ]);
+        $response->assertOk();
+        $this->assertDatabaseMissing('cart_product',[
+            'cart_id'=>$cart->id,
+            'product_id'=>$product->id
+        ]);
+        $this->assertDatabaseCount('cart_product',0);
+    }
+
+    public function test_product_added_to_cart_is_available_in_cart()
+    {
+        $user=User::factory()->create(['id'=>1]);
+        $this->actingAs($user);
+        $product=Product::factory()->create();
+        $response=$this->get('cart/product',[
+            'userId'=>$user->id,
+            'productId'=>$product->id
+        ]);
+        $response->assertOk()
+                 ->assertJsonCount(1,'productInCart');
+    }
+}
