490e5970238fb201fcd5290743bad4fdfcb7f0e2
ezdev21 <ezradev21@gmail.com>
2026-02-24 12:26:37 +0300

    Remove pyc files and update gitignore

--- server/products/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'description', 'price', 'quantity')
-        read_only_fields = ("id", "created_at", "updated_at", "user")
